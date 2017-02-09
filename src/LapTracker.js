a367ac23faedbbc28662fa5f2c8cc83fca34f203
Кузнецов Евгений <ek027823@gmail.com>
2017-02-09 16:33:35 +0300

    Structure is set.

--- src/LapTracker.js
+++ src/LapTracker.js
@@ -13,14 +13,37 @@ import {
 } from 'react-native';
 
 export default class LapTracker extends Component {
+    startStopButton() {
+        return <View style={styles.buttons}>
+                   <Text>Start</Text>
+               </View>
+    }
+    lapButton() {
+        return <View style={styles.buttons}>
+                   <Text>Lap</Text>
+               </View>
+    }
+
+    border(color) {
+        return {
+            borderColor: color,
+            borderWidth: 4
+        }
+    }
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.showPart}>
-
-        </View>
-
-        <View style={styles.managingPart}>
+          <View style={[styles.header, this.border('yellow')]}>
+            <View style={[styles.timerWrapper, this.border('red')]}>
+                <Text> 00:00.00</Text>
+            </View>
+            <View style={[styles.buttonWrapper, this.border('green')]}>
+                {this.startStopButton()}
+                {this.lapButton()}
+            </View>
+          </View>
+        <View style={[styles.footer, this.border('blue')]}>
 
         </View>
       </View>
@@ -31,16 +54,29 @@ export default class LapTracker extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'stretch',
         backgroundColor: 'blue'
     },
-    showPart: {
-        flex: 3,
+    header: {
+        flex: 1,
         backgroundColor: 'grey'
     },
-    managingPart: {
-        flex: 8,
+    footer: {
+        flex: 1,
         backgroundColor: 'pink'
-    }
+    },
+    timerWrapper: {
+        flex: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonWrapper: {
+        flex: 3,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+
 });
 
 AppRegistry.registerComponent('LapTracker', () => LapTracker);
